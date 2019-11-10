e2bd3c20670243157c1f3c9d68eb9f8294939e74
Sunny Sharma <sunnysharmagts@gmail.com>
2019-11-10 19:39:07 +0530

    Update to 0.0.1.dev0
    Update to dev version 0.0.1.dev0 which will be easier to handle in Pypi.

--- secretfy_template/__init__.py
+++ secretfy_template/__init__.py
@@ -1,5 +1,5 @@
 """Secretfy-template - Secrets config creator framework."""
 
 
-__version__ = '0.0.1.dev'
+__version__ = '0.0.1.dev0'
 __author__ = 'Secretfy-config-creator Authors and Contributors'
