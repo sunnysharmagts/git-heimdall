33ebe0c24bcd2f587cee0a66e6d11f8cf6fe4584
Sunny Sharma <sunnysharmagts@gmail.com>
2020-04-05 12:17:25 +0530

    Change secretfy to heimdall
    Change secretfy to heimdall in all source files and setup.py

--- secretfy_template/__init__.py
+++ secretfy_template/__init__.py
@@ -1,5 +1,5 @@
-"""Secretfy-template - Secrets config creator framework."""
+"""Git-Heimdall - Github gatekeeper framework."""
 
 
 __version__ = '0.0.1.alpha1'
-__author__ = 'Heimdall Authors and Contributors'
+__author__ = 'Git-Heimdall Authors and Contributors'
