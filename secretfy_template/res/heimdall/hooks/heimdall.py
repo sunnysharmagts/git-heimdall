20223d9e14201e7c89908af965a60bbb6bdc5d5b
Sunny Sharma <sunnysharmagts@gmail.com>
2020-04-05 10:39:21 +0530

    Add res for git hooks
        Delete df file
    m-C02WQ3H4HTD5:Secretfy-config-creator s0s0249$ clear
    Add res files for git hooks. It contains the following files :-
    
    - exclude/info : Info file for excluding from git.
    - hooks/pre-commit : pre-commit script which is invoked when any commit is
      pushed.
    - hooks/heimdall.py : This is invoked by pre-commit which then run the heimdall
      tool for scanning.

---
+++ secretfy_template/res/heimdall/hooks/heimdall.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import re
+#from termcolor import colored, cprint
+import time
+import threading
+import os
+
+def scan():
+    _pwd = sys.argv[1]
+    _files = sys.argv[2:]
+    _file_list = ' '.join(_files)
+    printDescription(_pwd, _file_list)
+    _marker = '~~~~~~~~~~~~~~'
+    p = subprocess.run(["heimdall","-scan", _pwd, _file_list], capture_output=True)
+    _byte_output = p.stdout
+    _output = _byte_output.decode()
+    _start_index = _output.find(_marker)
+    _final_output = _output[_start_index:].strip()
+    print('\n')
+    _exit_code = 0
+    if len(_final_output) > 0:
+        _exit_code = 1
+        #Ask user whether he wants to still resume with the commit
+        color_print('\x1b[91m','Your files has some sensitive data.','\x1b[0m\n')
+        print(_final_output)
+        _continue_with_commit = ''
+        sys.stdin = open('/dev/tty')
+        while len(_continue_with_commit) == 0:
+            _continue_with_commit=input('Do you want to continue with the commit ?(y/n): ')
+        _continue_with_commit = _continue_with_commit.lower()
+        if(_continue_with_commit == 'y' or _continue_with_commit == 'yes'):
+            _exit_code = 0
+        elif(_continue_with_commit == 'n' or _continue_with_commit == 'no'):
+            _exit_code = 1
+            revert_commit(_pwd)
+    else:
+        print('\x1b[92mCongratulations~!!!. There is no sensitive data in your files.\x1b[0m\n')
+    sys.exit(_exit_code)
+
+def color_print(start_tag, text, end_tag):
+    print(start_tag+text+end_tag)
+
+def revert_commit(root_dir):
+    p = subprocess.run(["cd", root_dir], capture_output=True)
+    p = subprocess.run(["git", "reset", "*"], capture_output=True)
+
+def printDescription(root, file_name_list):
+    print('#' * 100)
+    print('\n')
+    print('[INFO] Initiating the scanning process...')
+    print('\n')
+    print('#' * 100)
+    print('\n')
+    print('[INFO] Scanning the following files:\n')
+    file_name_list = file_name_list.split()
+    for file in file_name_list:
+        print('[INFO] ','.' * 20,os.path.join(root, file))
+    print('\n')
+    print('[INFO] This will take few seconds...')
+
+if __name__ == "__main__":
+    scan()
