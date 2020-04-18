450d9930f878bd890a4ef49d6e64c29f68619640
Sunny Sharma <sunnysharmagts@gmail.com>
2020-04-18 23:31:09 +0530

    Fix lint issues
    Fix lint issues

--- heimdall/res/heimdall/hooks/heimdall.py
+++ heimdall/res/heimdall/hooks/heimdall.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 import os
-import re
 import subprocess
 import sys
-import threading
-#from termcolor import colored, cprint
 import time
 
 
@@ -15,7 +12,8 @@ def scan():
     _file_list = ' '.join(_files)
     printDescription(_pwd, _file_list)
     _marker = '~~~~~~~~~~~~~~'
-    p = subprocess.run(["heimdall","-scan", _pwd, _file_list], capture_output=True)
+    p = subprocess.run(["heimdall", "-scan", _pwd, _file_list],
+                       capture_output=True)
     _byte_output = p.stdout
     _output = _byte_output.decode()
     _start_index = _output.find(_marker)
@@ -24,13 +22,15 @@ def scan():
     _exit_code = 0
     if len(_final_output) > 0:
         _exit_code = 1
-        #Ask user whether he wants to still resume with the commit
-        color_print('\x1b[91m','Your files has some sensitive data.','\x1b[0m\n')
+        # Ask user whether he wants to still resume with the commit
+        color_print('\x1b[91m', 'Your files has some sensitive \
+                    data.', '\x1b[0m\n')
         print(_final_output)
         _continue_with_commit = ''
         sys.stdin = open('/dev/tty')
         while len(_continue_with_commit) == 0:
-            _continue_with_commit=input('Do you want to continue with the commit ?(y/n): ')
+            _continue_with_commit = input('Do you want to continue with the \
+                                           commit ?(y/n): ')
         _continue_with_commit = _continue_with_commit.lower()
         if(_continue_with_commit == 'y' or _continue_with_commit == 'yes'):
             _exit_code = 0
@@ -38,15 +38,19 @@ def scan():
             _exit_code = 1
             revert_commit(_pwd)
     else:
-        print('\x1b[92mCongratulations~!!!. There is no sensitive data in your files.\x1b[0m\n')
+        print('\x1b[92mCongratulations~!!!. There is no sensitive data in your \
+               files.\x1b[0m\n')
     sys.exit(_exit_code)
+
 
 def color_print(start_tag, text, end_tag):
     print(start_tag+text+end_tag)
 
+
 def revert_commit(root_dir):
-    p = subprocess.run(["cd", root_dir], capture_output=True)
-    p = subprocess.run(["git", "reset", "*"], capture_output=True)
+    subprocess.run(["cd", root_dir], capture_output=True)
+    subprocess.run(["git", "reset", "*"], capture_output=True)
+
 
 def printDescription(root, file_name_list):
     print('#' * 100)
@@ -58,7 +62,7 @@ def printDescription(root, file_name_list):
     print('[INFO] Scanning the following files:\n')
     file_name_list = file_name_list.split()
     for file in file_name_list:
-        print('[INFO] ','.' * 20,os.path.join(root, file))
+        print('[INFO] ', '.' * 20, os.path.join(root, file))
     print('\n')
     print('[INFO] This will take few seconds...')
 
