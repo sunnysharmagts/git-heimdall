c7b92ecd823530f9ac5ccf3888aa164ca4f94496
Sunny Sharma <sunnysharmagts@gmail.com>
2020-04-20 10:14:02 +0530

    Add registration for repo
    Add registration mechanism for existing repositories to heimdall.

--- heimdall/codescan/manager.py
+++ heimdall/codescan/manager.py
@@ -8,7 +8,11 @@ module.
 """
 
 import fileinput
+import getpass
 import os
+import os.path as path
+import shutil
+import subprocess
 import sys
 
 from heimdall import config
@@ -48,3 +52,19 @@ class CodescanManager:
             for file in _file_name_list:
                 _files.append(os.path.join(_repo_abs_dir_path, file))
             self.plugin_manager.scan(_files)
+
+    def add(self, argv):
+        if argv:
+            for repo_path in argv:
+                if path.exists(repo_path):
+                    _repo_root_path = config.get_git_repo_path(repo_path)
+                    _repo_git_hook_path = path.join(_repo_root_path,'.git/hooks')
+                    _current_dir = config.get_config_path()
+                    _res_dir = path.join(_current_dir,'res/heimdall/hooks')
+                    _res_hook_files = os.listdir(_res_dir)
+                    for file in _res_hook_files:
+                        _file_name = path.join(_res_dir, file)
+                        _dest_file = path.join(_repo_git_hook_path, file)
+                        shutil.copyfile(_file_name, _dest_file)
+                        _relative_dest_file = path.join('.git/hooks', file)
+                        p = subprocess.run(["chmod", "a+x", _relative_dest_file], cwd=_repo_root_path, capture_output=False, shell=False)
